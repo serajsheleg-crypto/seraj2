50dad3646be8e53b1835337f8ea5e2dc8c209fbf
serajsheleg-crypto <serajsheleg@gmail.com>
2025-10-16 22:26:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// تمرير ناعم للزر
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
